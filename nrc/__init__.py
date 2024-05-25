2afe560e3a3f5f79b2f20831e9bf079f10c5f68d
David González Martínez <me@davidgonmar.me>
2024-05-25 06:10:01 +0200

    feat: organize, and better

--- nrc/__init__.py
+++ nrc/__init__.py
@@ -1,133 +1,8 @@
 import numpy as np
 import logging
-
+from nrc.fuzzycmeans import FuzzyCMeans
 logging.basicConfig(level=logging.INFO)
 
-
-class FuzzyCMeans:
-    def _euclidean_distance(self, x1: np.ndarray, x2: np.ndarray) -> float:
-        """
-        Compute the Euclidean distance between two vectors.
-        Args:
-            x1: (n_features,) ndarray
-            x2: (n_features,) ndarray
-
-        Returns:
-            float: the Euclidean distance between x1 and x2
-        """
-        return np.sqrt(
-            np.sum((x1 - x2) ** 2, axis=-1)
-        )  # sum over the features, not the samples
-
-    _dist = _euclidean_distance
-
-    def __init__(
-        self, n_clusters: int = 3, max_iter: int = 100, m: float = 2, eps: float = 0.01
-    ):
-        self.n_clusters = n_clusters
-        self.max_iter = max_iter
-        self.m = m
-        self.fitted = False
-        self.eps = eps
-
-    def _init_membership(self, n_samples: int) -> np.ndarray:
-        """
-        Initialize the membership matrix.
-        Args:
-            n_samples: int, the number of samples
-
-        Returns:
-            ndarray: the membership matrix of shape (n_clusters, n_samples)
-        """
-        r = np.random.rand(self.n_clusters, n_samples)
-        return r / r.sum(
-            0
-        )  # normalize the rows to sum to 1 (sum accross a column is 1)
-
-    def _compute_centroids(self, X: np.ndarray, membership: np.ndarray) -> np.ndarray:
-        """
-        Compute the centroids of the clusters.
-        Args:
-            X: (n_samples, n_features) ndarray
-            membership: (n_clusters, n_samples) ndarray, the membership matrix
-
-        Returns:
-            ndarray: the centroids of the clusters
-        """
-        return np.dot(membership**self.m, X) / np.sum(
-            membership**self.m, axis=1, keepdims=True
-        )
-
-    def _update_membership(self, X: np.ndarray, centroids: np.ndarray) -> np.ndarray:
-        """
-        Update the membership matrix.
-        Args:
-            X: (n_samples, n_features) ndarray
-            centroids: (n_clusters, n_features) ndarray
-
-        Returns:
-            ndarray: the updated membership matrix
-        """
-        distances = np.array(
-            [self._dist(X, c) for c in centroids]
-        )  # shape (n_clusters, n_samples)
-        dsum_per_cluster = (
-            distances.sum(0, keepdims=True) + 1e-8
-        )  # avoid div by zero, shape (1, n_samples)
-        normalized_dists = (
-            distances / dsum_per_cluster + 1e-8
-        )  # avoid div by zero, shape (n_clusters, n_samples)
-        return 1 / (normalized_dists ** (2 / (self.m - 1)))
-
-    def fit(self, X: np.ndarray):
-        """
-        Fit the model to the data matrix X.
-        Args:
-            X: (n_samples, n_features) ndarray
-
-        Returns:
-            self
-        """
-        assert (
-            len(X.shape) == 2
-        ), "X must be a 2D array with shape (n_samples, n_features), got shape {}".format(
-            X.shape
-        )
-        if self.fitted:
-            logging.warning(
-                "The model has already been fitted. Re-fitting will overwrite the previous model."
-            )
-
-        self.fitted = True
-        n_samples = X.shape[0]
-
-        # Initialize the centroids and labels
-        self.membership = self._init_membership(n_samples)
-        self.centroids = self._compute_centroids(X, self.membership)
-
-        for _ in range(self.max_iter):
-            self.centroids = self._compute_centroids(X, self.membership)
-            new_membership = self._update_membership(X, self.centroids)
-            if np.allclose(new_membership, self.membership, atol=self.eps):
-                logging.info("Converged after {} iterations. Stopping early.".format(_))
-                self.centroids = self._compute_centroids(
-                    X, new_membership
-                )  # update centroids one last time
-                break
-            self.membership = new_membership
-
-        return self
-
-    def get_hard_labels(self):
-        """
-        Get the hard labels of the data.
-        Returns:
-            ndarray: the hard labels of the data
-        """
-        assert self.fitted, "The model has not been fitted yet."
-        return np.argmax(
-            self.membership, axis=0
-        )  # get the index of the maximum value along the rows
 
 
 class NoisyRingsClustering:
@@ -254,8 +129,6 @@ class NoisyRingsClustering:
         alpha = np.maximum(1 - radii[..., None] / (
             center_dists + self.eps
         ), radii[:, None])  # shape (n_rings, n_samples)
-
-        print("alpha", alpha.shape, alpha)
         common = (self.memberships**self.q) * alpha  # shape (n_rings, n_samples)
 
         return np.sum(
@@ -299,7 +172,6 @@ class NoisyRingsClustering:
         )  # shape (n_rings, n_samples)
         # only take into account distances to the center of the cluster
         self.radii = np.mean(dist, axis=1)  # shape (n_rings)
-        print(dist, self.radii, self.centers)
 
     def fit(self, x: np.ndarray) -> None:
         assert x.ndim == 2, "Input data must be 2D, got shape {}".format(x.shape)
@@ -323,7 +195,7 @@ class NoisyRingsClustering:
             self.radii = self.get_new_radii(x)
             self.centers = self.get_new_centers(x)
 
-    def get_hard_labels(self) -> np.ndarray:
+    def get_labels(self) -> np.ndarray:
         """
         Get the hard labels of the samples
 
@@ -332,86 +204,4 @@ class NoisyRingsClustering:
             centers: array of shape (n_samples, n_features)
             memberships: array of shape (n_samples) with the index of the cluster for each sample
         """
-        return self.radii, self.centers, np.argmax(self.memberships, axis=0)
-
-
-if __name__ == "__main__":
-    while True:
-        def ds1():
-            # create a ring dataset
-            n_samples = 1000
-            n_features = 2
-            n_clusters = 2
-            point = []
-            POSRAND = 10
-
-            for i in range(n_clusters):
-                radius = 10 + np.random.uniform(-1, 1)
-                theta = np.linspace(0, 2 * np.pi, n_samples // n_clusters)
-                x = radius * np.cos(theta) + np.random.normal(
-                    0, 0.4, size=(n_samples // n_clusters)
-                )
-                y = radius * np.sin(theta) + np.random.normal(
-                    0, 0.4, size=(n_samples // n_clusters)
-                )
-                position = np.random.uniform(-POSRAND, POSRAND, size=(2))
-                x += position[0]
-                y += position[1]
-                point.append(np.stack([x, y], axis=1))
-
-            point = np.array(point)
-
-            # merge dim 0 and 1
-            point = point.reshape(-1, n_features)
-
-            return point
-
-        def ds2():
-            # just a point on [10, 10] with some noise and radius 3
-            n_samples = 1000
-            n_features = 2
-            n_clusters = 1
-            point = []
-            radius = 3
-            theta = np.linspace(0, 2 * np.pi, n_samples)
-            x = radius * np.cos(theta) + np.random.normal(0, 0.1, size=(n_samples))
-            y = radius * np.sin(theta) + np.random.normal(0, 0.1, size=(n_samples))
-            position = np.array([5, 5])
-            x += position[0]
-            y += position[1]
-            point.append(np.stack([x, y], axis=1))
-            point = np.array(point)
-
-            # merge dim 0 and 1
-            point = point.reshape(-1, n_features)
-
-            return point
-
-        point = ds1()
-
-        n_clusters = 2
-        ring_clustering = NoisyRingsClustering(
-            n_clusters, q=1.2, convergence_eps=0.0001, max_iters=100
-        )
-
-        ring_clustering.fit(point)
-
-        radii, centers, memberships = ring_clustering.get_hard_labels()
-
-        import matplotlib.pyplot as plt
-
-        plt.scatter(point[:, 0], point[:, 1], c=memberships)
-
-        # draw centers
-        plt.scatter(centers[:, 0], centers[:, 1], c="r", s=100, marker="x")
-
-        # draw radii
-
-        for i in range(n_clusters):
-            circle = plt.Circle(centers[i], radii[i], color="r", fill=False)
-            plt.gcf().gca().add_artist(circle)
-
-        plt.show()
-
-        print("done")
-    
+        return self.radii, self.centers, self.memberships
---
+++ nrc/fuzzycmeans.py
@@ -0,0 +1,127 @@
+import numpy as np
+import logging
+
+class FuzzyCMeans:
+    def _euclidean_distance(self, x1: np.ndarray, x2: np.ndarray) -> float:
+        """
+        Compute the Euclidean distance between two vectors.
+        Args:
+            x1: (n_features,) ndarray
+            x2: (n_features,) ndarray
+
+        Returns:
+            float: the Euclidean distance between x1 and x2
+        """
+        return np.sqrt(
+            np.sum((x1 - x2) ** 2, axis=-1)
+        )  # sum over the features, not the samples
+
+    _dist = _euclidean_distance
+
+    def __init__(
+        self, n_clusters: int = 3, max_iter: int = 100, m: float = 2, eps: float = 0.01
+    ):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.m = m
+        self.fitted = False
+        self.eps = eps
+
+    def _init_membership(self, n_samples: int) -> np.ndarray:
+        """
+        Initialize the membership matrix.
+        Args:
+            n_samples: int, the number of samples
+
+        Returns:
+            ndarray: the membership matrix of shape (n_clusters, n_samples)
+        """
+        r = np.random.rand(self.n_clusters, n_samples)
+        return r / r.sum(
+            0
+        )  # normalize the rows to sum to 1 (sum accross a column is 1)
+
+    def _compute_centroids(self, X: np.ndarray, membership: np.ndarray) -> np.ndarray:
+        """
+        Compute the centroids of the clusters.
+        Args:
+            X: (n_samples, n_features) ndarray
+            membership: (n_clusters, n_samples) ndarray, the membership matrix
+
+        Returns:
+            ndarray: the centroids of the clusters
+        """
+        return np.dot(membership**self.m, X) / np.sum(
+            membership**self.m, axis=1, keepdims=True
+        )
+
+    def _update_membership(self, X: np.ndarray, centroids: np.ndarray) -> np.ndarray:
+        """
+        Update the membership matrix.
+        Args:
+            X: (n_samples, n_features) ndarray
+            centroids: (n_clusters, n_features) ndarray
+
+        Returns:
+            ndarray: the updated membership matrix
+        """
+        distances = np.array(
+            [self._dist(X, c) for c in centroids]
+        )  # shape (n_clusters, n_samples)
+        dsum_per_cluster = (
+            distances.sum(0, keepdims=True) + 1e-8
+        )  # avoid div by zero, shape (1, n_samples)
+        normalized_dists = (
+            distances / dsum_per_cluster + 1e-8
+        )  # avoid div by zero, shape (n_clusters, n_samples)
+        return 1 / (normalized_dists ** (2 / (self.m - 1)))
+
+    def fit(self, X: np.ndarray):
+        """
+        Fit the model to the data matrix X.
+        Args:
+            X: (n_samples, n_features) ndarray
+
+        Returns:
+            self
+        """
+        assert (
+            len(X.shape) == 2
+        ), "X must be a 2D array with shape (n_samples, n_features), got shape {}".format(
+            X.shape
+        )
+        if self.fitted:
+            logging.warning(
+                "The model has already been fitted. Re-fitting will overwrite the previous model."
+            )
+
+        self.fitted = True
+        n_samples = X.shape[0]
+
+        # Initialize the centroids and labels
+        self.membership = self._init_membership(n_samples)
+        self.centroids = self._compute_centroids(X, self.membership)
+
+        for _ in range(self.max_iter):
+            self.centroids = self._compute_centroids(X, self.membership)
+            new_membership = self._update_membership(X, self.centroids)
+            if np.allclose(new_membership, self.membership, atol=self.eps):
+                logging.info("Converged after {} iterations. Stopping early.".format(_))
+                self.centroids = self._compute_centroids(
+                    X, new_membership
+                )  # update centroids one last time
+                break
+            self.membership = new_membership
+
+        return self
+
+    def get_hard_labels(self):
+        """
+        Get the hard labels of the data.
+        Returns:
+            ndarray: the hard labels of the data
+        """
+        assert self.fitted, "The model has not been fitted yet."
+        return np.argmax(
+            self.membership, axis=0
+        )  # get the index of the maximum value along the rows
